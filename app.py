5049935dda03a416673eb66d274820e3f81c53cc
lilis42 <lilistiyo9@gmail.com>
2023-06-07 11:25:44 +0700

    first commit

--- app.py
+++ app.py
@@ -1,19 +1,23 @@
 import os
+
 from os.path import join, dirname
+
 from dotenv import load_dotenv
 
 from flask import Flask, render_template, request, jsonify
 from pymongo import MongoClient
 
 dotenv_path = join(dirname(__file__), "env")
+
 load_dotenv(dotenv_path)
 
 MONGODB_URI = os.environ.get("MONGODB_URI")
+
 DB_NAME = os.environ.get("DB_NAME")
 
 client = MongoClient(MONGODB_URI) 
 
-db = client (DB_NAME)
+db = client[DB_NAME]
 
 app = Flask(__name__)
 
@@ -23,7 +27,7 @@ def home():
 
 @app.route("/homework", methods=["POST"])
 def homework_post():
-    # sample_receive = request.form['sample_give']
+
     name_receive = request.form['name_give']
     comment_receive = request.form['comment_give']
     doc = {
